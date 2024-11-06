21c164024bd38ffaa69c5eed1899df905911e18a
Ryan Brito <rm554497@fiap.com.br>
2024-11-06 18:01:02 -0300

    Primeiro Commit Arquivos Iniciais Imagens e App.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// Case Sensitive
+// Selecionando a tag "h1" com o querySelector
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do Número Secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um número entre 1 e 10";
+
+function verificarChute() {
+    console.log("O botão foi clicado!")
+}
